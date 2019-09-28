17c55ea082c228a548e3df883dbd1c822e2eee10
masterUNG <phrombutr@gmail.com>
2019-09-28 15:30:27 +0700

    Design UI Authen Finish

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -9,21 +9,98 @@ class _AuthenState extends State<Authen> {
   // Explicit
 
   // Method
+
+  Widget showLogo() {
+    return Container(
+      width: 150.0,
+      height: 150.0,
+      child: Image.asset('images/logo.png'),
+    );
+  }
+
   Widget showAppName() {
-    return Text('Ung Food Mall');
+    return Text(
+      'Ung Food Mall',
+      style: TextStyle(
+        fontSize: 30.0,
+        fontWeight: FontWeight.bold,
+        fontStyle: FontStyle.italic,
+        color: Colors.red[900],
+        fontFamily: 'Vibes',
+      ),
+    );
+  }
+
+  Widget userText() {
+    return Container(
+      width: 200.0,
+      child: TextFormField(
+        decoration: InputDecoration(labelText: 'User :'),
+      ),
+    );
+  }
+
+  Widget passwordText() {
+    return Container(
+      width: 200.0,
+      child: TextFormField(
+        obscureText: true,
+        decoration: InputDecoration(labelText: 'Password :'),
+      ),
+    );
+  }
+
+  Widget signInButton() {
+    return RaisedButton(color: Colors.red[900],
+      child: Text('Sign In', style: TextStyle(color: Colors.white),),
+      onPressed: () {},
+    );
+  }
+
+  Widget signUpButton() {
+    return OutlineButton(
+      child: Text('Sign Up'),
+      onPressed: () {},
+    );
+  }
+
+  Widget showButton() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        signInButton(),
+        SizedBox(
+          width: 4.0,
+        ),
+        signUpButton(),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              showAppName(),
-              showAppName(),
-            ],
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: RadialGradient(
+              colors: [Colors.white, Colors.yellow[600]],radius: 1.0,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                showLogo(),
+                showAppName(),
+                userText(),
+                passwordText(),
+                SizedBox(
+                  height: 10.0,
+                ),
+                showButton(),
+              ],
+            ),
           ),
         ),
       ),
