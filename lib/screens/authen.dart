eff4ecc91895a7d8ff867615dfe3a7e1e8ecd564
masterUNG <phrombutr@gmail.com>
2019-09-28 13:56:17 +0700

    Center Group Success

---
+++ lib/screens/authen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Authen extends StatefulWidget {
+  @override
+  _AuthenState createState() => _AuthenState();
+}
+
+class _AuthenState extends State<Authen> {
+  // Explicit
+
+  // Method
+  Widget showAppName() {
+    return Text('Ung Food Mall');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              showAppName(),
+              showAppName(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
