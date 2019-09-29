dd1211abfc750927d32b26186afc393d254228be
masterUNG <phrombutr@gmail.com>
2019-09-29 09:50:24 +0700

    Desing UI Register Finish

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -6,13 +6,71 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  // Explicit
+
+  // Method
+  Widget registerButton() {
+    return IconButton(
+      icon: Icon(Icons.cloud_upload),
+      onPressed: () {},
+    );
+  }
+
+  Widget nameText() {
+    return TextFormField(
+      decoration: InputDecoration(
+        icon: Icon(
+          Icons.fingerprint,
+          size: 36.0,
+        ),
+        labelText: 'Display Name :',
+        helperText: 'Type Your Name',
+      ),
+    );
+  }
+
+  Widget userText() {
+    return TextFormField(
+      decoration: InputDecoration(
+        icon: Icon(
+          Icons.account_box,
+          size: 36.0,
+        ),
+        labelText: 'User :',
+        helperText: 'Type Your User',
+      ),
+    );
+  }
+
+  Widget passwordText() {
+    return TextFormField(
+      decoration: InputDecoration(
+        icon: Icon(
+          Icons.lock,
+          size: 36.0,
+        ),
+        labelText: 'Password :',
+        helperText: 'Type Your Password',
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.yellow.shade900,
         title: Text('Regisger'),
+        actions: <Widget>[registerButton()],
       ),
-      body: Text('body'),
+      body: ListView(
+        padding: EdgeInsets.all(50.0),
+        children: <Widget>[
+          nameText(),
+          userText(),
+          passwordText(),
+        ],
+      ),
     );
   }
 }
